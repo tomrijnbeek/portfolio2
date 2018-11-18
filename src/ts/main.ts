93a6fcff03a2fe88f5f8984692982adb09c752bb
Tom <tom@tomrijnbeek.nl>
2018-11-18 15:19:10 +0000

    Migrate to webpack

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,8 @@
-import * as $ from 'jquery';
-import { sayHello } from './greet';
+import * as $ from "jquery";
+import { sayHello } from "./greet";
 
 function showHello(divName: string, name: string) {
-  $('#' + divName).html(sayHello(name));
+  $("#" + divName).html(sayHello(name));
 }
 
 showHello("greeting", "TypeScript");
