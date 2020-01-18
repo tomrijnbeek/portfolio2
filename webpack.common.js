94da993fcbdcc6b04d1f121b448941ee6f71319b
Tom Rijnbeek <tom@tomrijnbeek.nl>
2020-01-18 17:37:11 +0000

    SEO improvements and robots.txt

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.ts',
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RobotstxtPlugin = require('robotstxt-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -12,5 +13,14 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css'
     }),
+    new RobotstxtPlugin({
+      policy: [
+        {
+          userAgent: "*",
+          allow: "/",
+          crawlDelay: 2
+        },
+      ]
+    })
   ]
 });
